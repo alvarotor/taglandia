47d89f9813e8326beb12139126304fc7fe1df5d3
Alvaro Torpedo <goodbytes23@yahoo.es>
2017-02-12 23:27:32 +0200

    Changes on security

--- public/scripts/add.js
+++ public/scripts/add.js
@@ -70,8 +70,6 @@ function saveElement() {
         var tag = item.split("###");
         tags[tag[0]] = tag[1] === undefined ? "" : tag[1];
     });
-    // console.log(tags)
-    // console.log(Object.keys(tags))
     var name = document.getElementById("name").value;
     var description = document.getElementById("description").value;
     if (name === null || description === null || name.trim() === "" || description.trim() === "") {
@@ -93,13 +91,12 @@ function saveElement() {
         name: document.getElementById("name").value,
         description: document.getElementById("description").value,
         expire: moment(boxDate).utc().format(format),
-        tags: tags,
+        tags,
         user: uid,
         pictureURL
     };
     for (tag in tags)
         newElement["tags/en/" + tag + "/elements/" + newElementKey] = document.getElementById("name").value;
-    // console.log(newElement)
     db.ref().update(newElement, cleanForm);
 }
 
@@ -116,13 +113,12 @@ function cleanForm() {
     document.getElementById("progress-bar").value = 0;
     document.getElementById("picsMessage").textContent = "Upload a picture.";
     $("#success").html("<div class='alert alert-success'>");
-    $("#success > .alert-success").html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-        .append("</button>");
+    $("#success > .alert-success").html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;").append("</button>");
     $("#success > .alert-success").append("<strong>Element added.</strong>");
     $("#success > .alert-success").append("</div>");
 };
 
-$('#expiredate').datetimepicker({
+$("#expiredate").datetimepicker({
     format: "YYYY/MM/DD HH:mm"
 });
 
